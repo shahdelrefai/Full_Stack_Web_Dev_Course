10bdddc452c2da467127544c80cc656b4a411808
shahdelrefai <el.refai.shahd@gmail.com>
2025-02-25 17:48:20 +0200

    let examples

---
+++ let.js
@@ -0,0 +1,20 @@
+//----------------------let----------------------
+// let is block scoped
+// let can't be re-declared
+// let can be updated
+// let is not hoisted
+// let is not accessible outside the block
+let name = "Mohamed";
+let x = 10;
+console.log(x);
+
+if (true) {
+    let age = 30;
+    x = 55;
+}
+
+//console.log(age); // ReferenceError: age is not defined
+console.log(x); // 10 (Change is accessible outside the block)
+
+//console.log(y);
+let y = 20; // ReferenceError: Cannot access 'y' before initialization 
