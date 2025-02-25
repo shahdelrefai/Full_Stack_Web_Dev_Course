9a097037bed876044d282fda130576cd3449e67c
shahdelrefai <el.refai.shahd@gmail.com>
2025-02-25 17:48:11 +0200

    var examples

---
+++ var.js
@@ -0,0 +1,18 @@
+//----------------------var----------------------
+// var is function scoped
+// var can be re-declared
+// var can be updated
+// var is hoisted
+// var is accessible outside the block
+console.log(name); // Undefined (Hoisting)
+var name = "Mohamed";
+console.log(name); // Mohamed
+var x = 10;
+
+if (true) {
+    var age = 30;
+    x = 55;
+}
+
+console.log(age); // 30 (Accessible outside the block)
+console.log(x); // 55 (Change is accessible outside the block)
