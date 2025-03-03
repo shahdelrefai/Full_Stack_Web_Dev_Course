92d37233ae4e6f248cc47dd5d2e08de8e589e07d
shahdelrefai <el.refai.shahd@gmail.com>
2025-03-03 13:06:04 +0200

    different types of loops

---
+++ Javascriptt/Session 1/loops.js
@@ -0,0 +1,34 @@
+//----------------- For Loop -----------------
+// for (initialization; condition; increment/decrement)
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+//---------------- While Loop ----------------
+let count = 0;
+while (count < 5) {
+    console.log(count);
+    count++;
+}
+
+//--------------- Do-While Loop ---------------
+let num = 0;
+do {
+    console.log(num);
+    num++;
+} while (num < 5);
+
+//--------------- For-each Loop ---------------
+let colors = ["Red", "Green", "Blue"];
+colors.forEach((color) => {
+    console.log(color);
+});
+
+//--------------- For-in Loop ---------------
+let person = {
+    name: "John",
+    age: 30
+};
+for (let key in person) {
+    console.log(key, person[key]);
+}
