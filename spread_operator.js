9ac2cefeef5341d8bfb2b6c2435c6ca1268ea23d
shahdelrefai <el.refai.shahd@gmail.com>
2025-03-03 14:25:07 +0200

    comments edit

--- spread_operator.js
+++ spread_operator.js
@@ -1,17 +1,17 @@
 //The spread operator (...) allows an iterable to expand in places where
 // multiple arguments or elements are expected.
 
-//-------------On Lists-------------
+//-------------With Lists-------------
 let numbers = [1, 2, 3];
 let newNumbers = [...numbers, 4, 5];
 console.log(newNumbers); // [1, 2, 3, 4, 5]
 
-//-------------On Objects-------------
+//-------------With Objects-------------
 let person = { name: "Alice", age: 25 };
 let newPerson = { ...person, city: "New York" };
 console.log(newPerson); // { name: "Alice", age: 25, city: "New York" }
 
-//-------------On Functions-------------
+//-------------With Variables-------------
 function sum(a, b, c) {
     return a + b + c;
 }
