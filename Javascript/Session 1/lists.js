5a4cc4184a2c6955ca7ab64159f334b57bb6c449
shahdelrefai <el.refai.shahd@gmail.com>
2025-03-03 13:23:20 +0200

    added comments

--- Javascript/Session 1/lists.js
+++ Javascript/Session 1/lists.js
@@ -1,6 +1,7 @@
 let places = ["Dubai", "Riyadh", "Cairo"]; // Array of strings
 console.log(places[0]); // Dubai
 
+//----------------- List Methods -----------------
 places.push("Beirut"); // Add to end
 places.pop(); // Remove last item
 places.unshift("Amman"); // Add to start
@@ -16,4 +17,5 @@ places.join(", "); // Convert to string
 places.slice(1, 3); // Extract elements
 places.concat(["Muscat", "Manama"]); // Merge arrays
 
+//----------------- Mixed Array -----------------
 let mixedArray = ["Cairo", 5, true, null];
